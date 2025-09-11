b0f1e70607e2ef656adc98731f0eff1916402e7f
BodhiLaughton <bodhi.laughton@gmail.com>
2025-09-11 10:03:43 -0500

    Re-enable telemetry output for AprilTag  BODHI
    The telemetry lines displaying AprilTag detection information (ID, name, XYZ, PRY, RBE) were previously commented out. This change re-enables this telemetry output.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTags.java
@@ -104,12 +104,12 @@ public class AprilTags extends LinearOpMode {
                                     ArtifactColors.GREEN
                             };
                     }
-                    /*
+
                     telemetry.addLine(String.format("\n==== ID %d (%s)", detection.id, detection.metadata.name));
                     telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
                     telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                     telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
-                    */
+
                 } else {
                     /*
                     telemetry.addLine(String.format("\n==== ID %d (Unknown)", detection.id));
